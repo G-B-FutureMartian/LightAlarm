d510b1935019e68f8bf11b35ce189805873bf96c
G-B-FutureMartian <81188293+G-B-FutureMartian@users.noreply.github.com>
2022-08-19 17:08:11 -0400

    added areas to add functions.

--- Main.py
+++ Main.py
@@ -1,2 +1,35 @@
-import RPI.gpio as GPIO
+import RPI.gpio as gpio
 import time
+
+def setup():	# Setup for the GPIO pins on the raspberry pi
+	
+
+def alarmSet():	# A function for setting the alarm
+	
+
+def alarm():	# The code for the alarm going off
+	
+
+def preAlarm():	# The code for preparing for the alarm to go off
+	
+
+def snooze():	# The function for snoozing the alarm
+	
+
+def postAlarm():	# The code for when you don't snoooze/stop the alarm
+	
+
+def wait(): # The code to wait for the alarm time
+	
+
+def destroy():	# What happens when you kill the program via ctrl c
+  gpio.cleanup()
+
+if __name__ == '__main__':
+	setup():
+		try:
+			alarmSet()
+			wait()
+			alarm()
+		except KeyboardInterrupt: # KeyboardInterrupt = ctrl c
+			destroy()
